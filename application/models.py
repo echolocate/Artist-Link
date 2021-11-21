64577c181aa3aff07cf39f9d378113fdf392a76d
Mark Dance <markpdance@yahoo.co.uk>
2021-11-21 13:15:39 +0000

    added Agent class to models.py

--- application/models.py
+++ application/models.py
@@ -5,4 +5,15 @@ class Band(db.Model):
     band_name = db.Column(db.String(70), nullable=False)
     band_email = db.Column(db.String(50), nullable=False)
     band_phone = db.Column(db.Integer(20))
+    band_genre = db.Column(db.String(20))
+    agent_id = db.Column(db.Integer, db.ForeignKey('Agent.id'), nullable=False)
+
+class Agent(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    agent_name = db.Column(db.String(70), nullable=False)
+    agent_email = db.Column(db.String(50), nullable=False)
+    agent_phone = db.Column(db.Integer(20))
+    band = db.relationship('Band', backref='agentbr')
+
+
     
